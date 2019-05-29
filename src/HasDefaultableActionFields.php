ca438adfca3ca0ad25fdfd7570abb110cf8d1c68
Kit Burton-Senior <mail@kitbs.com>
2019-05-29 15:25:33 +0100

    add refreshIndex

--- src/HasDefaultableActionFields.php
+++ src/HasDefaultableActionFields.php
@@ -2,8 +2,18 @@
 
 namespace Inspheric\NovaDefaultable;
 
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use Illuminate\Support\Str;
+
+use Laravel\Nova\Actions\Action;
+
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Fields\ActionFields;
+
+use Laravel\Nova\Http\Requests\ActionRequest;
+
+use Laravel\Nova\Nova;
 
 trait HasDefaultableActionFields
 {
@@ -19,15 +29,35 @@ trait HasDefaultableActionFields
     {
         collect($this->fields())->filter(function($field) {
             return $field->meta['defaultLast'] ?? false;
-        })->each(function($field) use ($fields) {            
-            
+        })->each(function($field) use ($fields) {
+
             $field->withMeta(['value' => $fields->{$field->attribute}]);
             $request = app(NovaRequest::class);
-            
+
             DefaultableField::cacheLastValue($request, $field, $this->uriKey());
-            
+
         });
 
         return parent::handleResult($fields, $results);
+    }
+
+    /**
+     * Provide the ability to refresh the index page when an action is run,
+     * so that the action fields' default values can be repopulated.
+     *
+     * @return array|null
+     */
+    protected function refreshIndex()
+    {
+        if (!$this instanceof ShouldQueue) {
+            $request = app(ActionRequest::class);
+
+            $referrer = $request->header('Referer');
+            $uriKey = Nova::newResourceFromModel($request->targetModel())->uriKey();
+
+            if (Str::endsWith($referrer, '/'.$uriKey)) {
+                return Action::redirect($referrer);
+            }
+        }
     }
 }
